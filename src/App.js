2ce317af2e0fe5a2c13c5c4bb8f9d323b6f96ebc
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-04-12 00:12:31 +0000

    jeuDe hadoka fi dahnik Gambool

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { Component } from 'react'
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: 0 };
-    }
-    inc = () => {
-        this.setState({ name: this.state.name + 1 })
-    };
-    dec = () => {
-        this.setState({ name: this.state.name - 1 })
-    }
-    render() {
-        return (
-            <div className='App'>
-                <p>{this.state.name}</p>
-                <button onClick={this.inc} >Incrémenter</button>
-                <button onClick={this.dec}>Décrementer</button>
-            </div>
-        )
-    }
-}
+import React from 'react'
+import JeuDe from './components/JeuDe'
+export default function App() {
+    return (
+        <div>
+            <JeuDe cache={5} />
+        </div>
+    )
+};
