da77684a0f91092cf2bded35f2b6faf78ea1e607
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-04-16 14:30:44 +0000

    validé formulaire

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ap from './Ap';
+import App from './App';
+import Appp from './Appp';
+import Apppp from './Apppp';
+import Appppp from './Appppp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Ap />
+    <App />
+    <Appp />
+    <Apppp />
+    <Appppp />
+  </React.StrictMode>
+);
