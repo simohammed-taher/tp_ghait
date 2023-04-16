329276a643d3d073b4b81b5d04612b7a97bf51b4
MOHAMMED TAHER <100471051+simohammed-taher@users.noreply.github.com>
2023-04-16 15:09:17 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './Ap';
 import App from './App';
 import Appp from './Appp';
 import Apppp from './Apppp';
@@ -8,6 +9,7 @@ import Appppp from './Appppp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Ap />
     <App />
     <Appp />
     <Apppp />
